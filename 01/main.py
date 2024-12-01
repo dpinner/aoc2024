6ba7c22f1cc54de39310fb46c89a71d68a1b5c9b
dpinner <66742635+dpinner@users.noreply.github.com>
2024-12-01 12:38:21 -0800

    cleanup

--- 01/main.py
+++ 01/main.py
@@ -1,4 +1,17 @@
 import sys
+from collections import defaultdict
+from typing import Iterator, List, Tuple
+
+def get_dist(input: Tuple[Tuple[int,...], Tuple[int,...]]) -> int:
+    # sort and transpose back
+    sorted_lists = zip(*(sorted(l) for l in input))
+    return sum(abs(b-a) for a,b in sorted_lists)
+
+def get_similarity(input: Tuple[Tuple[int,...], Tuple[int,...]]) -> int:
+    freqs = defaultdict(int)
+    for v in input[1]:
+        freqs[v] += 1
+    return sum(v * freqs[v] for v in input[0])
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -6,13 +19,15 @@ if __name__ == "__main__":
     with open(filename,'r') as f:
         for l in f.read().splitlines():
             a,b = l.split()
-            tmp.append([int(a),int(b)])
-
+            tmp.append((int(a),int(b)))
+    
     # transpose
-    lists = list(zip(*tmp))
-    # sort and transpose back
-    sorted_lists = list(zip(*(sorted(l) for l in lists)))
-    dist = sum(abs(b-a) for a,b in sorted_lists)
-    print(dist)
+    lists = tuple(zip(*tmp))
+
+    # part 1
+    print(get_dist(lists))
+
+    # part 2
+    print(get_similarity(lists))
     
 
