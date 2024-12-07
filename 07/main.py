45b9c184b56b24a6abe13c3f0657956ce667e928
dpinner <66742635+dpinner@users.noreply.github.com>
2024-12-07 12:52:33 -0800

    day 7

---
+++ 07/main.py
@@ -0,0 +1,56 @@
+import sys
+from math import log
+from typing import List, Set
+
+op_map = {
+    "*": lambda a, b: a * b,
+    "+": lambda a, b: a + b,
+    "||": lambda a, b: int(str(a) + str(b)),
+}
+
+
+def is_valid(target: int, vals: List[int], operators: Set[str]) -> bool:
+    if len(vals) == 0:
+        return target == 0
+
+    if len(vals) == 1:
+        return target == vals[0]
+
+    if (
+        "+" in operators
+        and target >= vals[-1]
+        and is_valid(target - vals[-1], vals[:-1], operators)
+    ):
+        return True
+
+    if (
+        "*" in operators
+        and target % vals[-1] == 0
+        and is_valid(target // vals[-1], vals[:-1], operators)
+    ):
+        return True
+
+    if "||" in operators:
+        digits = int(1 + log(vals[-1], 10))
+        if (
+            target > vals[-1]
+            and int(str(target)[-digits:]) == vals[-1]
+            and is_valid(int(str(target)[:-digits]), vals[:-1], operators)
+        ):
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    calibs = []
+    with open(filename, "r") as f:
+        for l in f.read().splitlines():
+            target, vals = l.split(":")
+            vals_list = list(map(int, vals.split()))
+
+            calibs.append((int(target), vals_list))
+
+    print(sum(x[0] for x in calibs if is_valid(*x, ["+", "*"])))
+    print(sum(x[0] for x in calibs if is_valid(*x, ["+", "*", "||"])))
